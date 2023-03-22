1af9efe31baf75357ba1495b51ec7e1c3f9d8343
scott1ee <franhe345@gmail.com>
2023-03-22 12:58:45 -0300

    agregando nav y section

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './component/navbar/navbar.component';
+import { SectionComponent } from './component/section/section.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    SectionComponent
   ],
   imports: [
     BrowserModule,
