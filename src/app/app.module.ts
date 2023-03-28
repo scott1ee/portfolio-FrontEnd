e031c07e33d57e0856da9bbf8c83ca8c41f839e5
scott1ee <franhe345@gmail.com>
2023-03-28 13:22:34 -0300

    agrege componente education

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,20 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './component/navbar/navbar.component';
-import { SectionComponent } from './component/section/section.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { FooterComponent } from './component/footer/footer.component';
 import { ProjectsComponent } from './component/projects/projects.component';
 import { BannerComponent } from './component/banner/banner.component';
+import { EducationComponent } from './component/education/education.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    SectionComponent,
     FooterComponent,
     ProjectsComponent,
-    BannerComponent
+    BannerComponent,
+    EducationComponent
   ],
   imports: [
     BrowserModule,
