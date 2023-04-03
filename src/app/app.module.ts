fa45f6b6ce6c7ea38bdeaa3d2e2f0d1459956e4b
scott1ee <franhe345@gmail.com>
2023-04-03 14:55:04 -0300

    projects finalizado

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,30 +1,28 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './component/navbar/navbar.component';
-import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-import { FooterComponent } from './component/footer/footer.component';
-import { ProjectsComponent } from './component/projects/projects.component';
 import { BannerComponent } from './component/banner/banner.component';
 import { EducationComponent } from './component/education/education.component';
+import { ProjectsComponent } from './component/projects/projects.component';
 import { SkillsComponent } from './component/skills/skills.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    FooterComponent,
-    ProjectsComponent,
     BannerComponent,
     EducationComponent,
+    ProjectsComponent,
     SkillsComponent
   ],
   imports: [
     BrowserModule,
     FontAwesomeModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
